c18134cd93775ec2b2bb51ec6648c6bb2f34142f
aomnes <antoine.omnes@utbm.fr>
2017-01-28 21:02:44 +0100

    acl

---
+++ acl.c
@@ -0,0 +1,27 @@
+#include "header.h"
+
+void acl(char *filename)
+{
+    acl_t acl = NULL;
+    acl_entry_t dummy;
+    ssize_t xattr = 0;
+    char str[10];
+
+    acl = acl_get_link_np(filename, ACL_TYPE_EXTENDED);
+    if (acl && acl_get_entry(acl, ACL_FIRST_ENTRY, &dummy) == -1)
+    {
+        acl_free(acl);
+        acl = NULL;
+    }
+    xattr = listxattr(filename, NULL, 0, XATTR_NOFOLLOW);
+    if (xattr < 0)
+        xattr = 0;
+    str[1] = '\0';
+    if (xattr > 0)
+        str[0] = '@';
+    else if (acl != NULL)
+        str[0] = '+';
+    else
+        str[0] = ' ';
+    printf("%s", str);
+}
